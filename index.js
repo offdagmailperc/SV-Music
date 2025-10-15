9ee6fd77bc1f9733a34d8929540b40c77018727f
Fiend <bigtopsyturvy@gmail.com>
2025-10-15 15:08:54 +0100

    moved some stuff around

---
+++ index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAnzBwC2z-fNw2o2fpaRaMS_rbWP8js1Pg",
+
+    authDomain: "sv-music-415ed.firebaseapp.com",
+
+    projectId: "sv-music-415ed",
+
+    storageBucket: "sv-music-415ed.firebasestorage.app",
+
+    messagingSenderId: "717364474819",
+
+    appId: "1:717364474819:web:b69340f30ae7e5155c0376"
+
+};
+const app = initializeApp(firebaseConfig);
