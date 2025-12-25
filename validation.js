3962acf4c378a7509ce3539de2a41db8000b05c4
Fiend <bigtopsyturvy@gmail.com>
2025-12-25 14:35:14 +0000

    updated css and  whatnot

---
+++ validation.js
@@ -0,0 +1,75 @@
+const form = document.getElementById('form')
+const username_input = document.getElementById('username-input')
+const password_input = document.getElementById('password-input')
+const repeat_password_input = document.getElementById('repeat-password-input')
+const error_message = document.getElementById('error-message')
+
+form.addEventListener('submit', (e) => {
+  let errors = []
+
+  if(repeat_password_input){
+    // If we have a repeat password input then we are in the signup
+    errors = getSignupFormErrors(firstname_input.value, password_input.value, repeat_password_input.value)
+  }
+  else{
+    // If we don't have a repeat password input then we are in the login
+    errors = getLoginFormErrors(username_input.value, password_input.value)
+  }
+
+  if(errors.length > 0){
+    // If there are any errors
+    e.preventDefault()
+    error_message.innerText  = errors.join(". ")
+  }
+})
+
+function getSignupFormErrors(username, password, repeatPassword){
+  let errors = []
+
+  if(username === '' || username == null){
+    errors.push('Username is required')
+    username_input.parentElement.classList.add('incorrect')
+  }
+  if(password === '' || password == null){
+    errors.push('Password is required')
+    password_input.parentElement.classList.add('incorrect')
+  }
+  if(password.length < 8){
+    errors.push('Password must have at least 8 characters')
+    password_input.parentElement.classList.add('incorrect')
+  }
+  if(password !== repeatPassword){
+    errors.push('Password does not match repeated password')
+    password_input.parentElement.classList.add('incorrect')
+    repeat_password_input.parentElement.classList.add('incorrect')
+  }
+
+
+  return errors;
+}
+
+function getLoginFormErrors(username, password){
+  let errors = []
+
+  if(username === '' || email == null){
+    errors.push('Email is required')
+    username_input.parentElement.classList.add('incorrect')
+  }
+  if(password === '' || password == null){
+    errors.push('Password is required')
+    password_input.parentElement.classList.add('incorrect')
+  }
+
+  return errors;
+}
+
+const allInputs = [username_input, password_input, repeat_password_input].filter(input => input != null)
+
+allInputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if(input.parentElement.classList.contains('incorrect')){
+      input.parentElement.classList.remove('incorrect')
+      error_message.innerText = ''
+    }
+  })
+})
